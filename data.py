281aaf650cb0c2bbffb9057a9da293af10f1d0ff
Pavitra Dangati <pavitradangati@Pavitras-MacBook-Pro.local>
2020-04-19 00:38:35 -0400

    Adding normalisation

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pathlib import Path
 import numpy as np
+from sklearn.preprocessing import RobustScaler
 
 KNOBS = [f'k{i}' for i in range(1, 9)] + [f's{i}' for i in range(1, 5)]
 LATENCY = 'latency'
@@ -10,24 +11,23 @@ METRICS_START = 13
 def data(name: str) -> pd.DataFrame:
     input_data = Path(f'.data/{name}.CSV')
     inp_data = pd.read_csv(input_data)
-
     return inp_data
 
 
 def for_fa(all_data: pd.DataFrame,
-           metrics_start: int = METRICS_START) -> np.ndarray:
+           metrics_start: int = METRICS_START,
+           normalise: bool = True) -> np.ndarray:
     """
         Args:
             all_data: Complete DataFrame
-
             metrics_start: Column number from which the metrics start (def: 13)
-
         Returns:
             numpy.ndarray of shape (num_metrics, num_configs)
-
     """
-
-    return (all_data[all_data.columns[metrics_start:]]).values.T
+    x = (all_data[all_data.columns[metrics_start:]]).values
+    if normalise:
+        x = RobustScaler(copy=False).fit(x)
+    return x.T
 
 
 if __name__ == '__main__':
--- fa_wandb.py
+++ fa_wandb.py
@@ -13,18 +13,20 @@ import logging
 from data import data, for_fa
 from pathlib import Path
 import wandb 
+
 #from clustering import KMeans
 logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def compute_score(X, args):
     fa = FactorAnalysis(n_components=args.ncomponents)
+
     return np.mean(cross_val_score(fa, X, verbose=2, cv=args.folds, n_jobs=args.workers))
 
 if __name__=='__main__':
     wandb.init()
     parser = argparse.ArgumentParser()
-    parser.add_argument('--ncomponents', type=int, default=20,
+    parser.add_argument('--ncomponents', type=int, required=True,
             help='Number of components param to be passed to FA.')   
     parser.add_argument('--workers', type=int, default=3,
             help='pool size for multiprocessing')
@@ -35,4 +37,4 @@ if __name__=='__main__':
     X = for_fa(data('offline_workload'))
     score = compute_score(X, args)
     wandb.log({'score': score})
-
+    logging.info(f'{args.ncomponents} : {score}')
