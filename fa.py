49934648b1fe11283d78915e678ce80554501bff
Pavitra Dangati <pavitradangati@Pavitras-MacBook-Pro.local>
2020-04-18 21:29:15 -0400

    Adding params: folds

--- fa.py
+++ fa.py
@@ -12,24 +12,24 @@ import argparse
 import logging 
 from data import data, for_fa
 from pathlib import Path
+#from clustering import KMeans
 logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def compute_score(X, n_components):
+def compute_score(X, folds,  n_components):
     fa = FactorAnalysis(n_components=n_components)
-    return np.mean(cross_val_score(fa, X))
+    return np.mean(cross_val_score(fa, X, verbose=2, cv=folds))
 
-def search(X, max_components ,step_size, chunk_size, pool_workers):
-    score = partial(compute_score, X)
+def search(X, folds, max_components ,step_size, chunk_size, pool_workers):
+    score = partial(compute_score, X, folds)
     grid = list(range(1, max_components, step_size))
     with multiprocessing.Pool(pool_workers) as pool:
-        results = list(tqdm.tqdm(pool.imap(score, 
-        grid,chunksize=chunk_size), total=len(grid)))
+        results = list(tqdm.tqdm(pool.imap(score,grid,chunksize=chunk_size), total=len(grid)))
     return grid[np.argmax(results)]
 
-def fa(x, max_components=1000,step_size=1, chunk_size=2, pool_workers=5):
-    x = np.asarray(x)
-    n_components_fa = search(x, max_components, step_size=step_size, chunk_size=chunk_size, pool_workers=pool_workers)
+def fa(x, folds=3, max_components=1000,step_size=1, chunk_size=2, pool_workers=5):
+    max_components = min(max_components, x.shape[-1])
+    n_components_fa = search(x, folds, max_components, step_size=step_size, chunk_size=chunk_size, pool_workers=pool_workers)
     logger.info('n_components {}'.format(n_components_fa))
     transformer = FactorAnalysis(n_components=n_components_fa, random_state=123)
     X_transformed = transformer.fit_transform(x)
@@ -44,12 +44,18 @@ if __name__=='__main__':
     parser.add_argument('--chunk', type=int, default=2,
             help='chunk size for imap multiptocessing') 
     parser.add_argument('--workers', type=int, default=5,
-            help='pool size for multiprocessing')      
+            help='pool size for multiprocessing')
+    parser.add_argument('--folds', type=int, default=3,
+            help='K for Kfolds')    
     parser.add_argument('--output', type=Path, default='output',
-            help='Filepath to save transformed X')                          
+            help='Filepath to save transformed X')        
+
+
     args = parser.parse_args()
+    #X,_ = load_digits(return_X_y=True)
+    logging.info('Loading data!')
     X = for_fa(data('offline_workload'))
-    X_transformed = fa(X, max_components=args.max_comp, step_size=args.step, chunk_size=args.chunk, pool_workers=args.workers)
+    X_transformed = fa(X, folds=args.folds, max_components=args.max_comp, step_size=args.step, chunk_size=args.chunk, pool_workers=args.workers)
     np.save(args.output.with_suffix('.npy'), X_transformed)
     logger.info('Saved x_transformed to {}'.format(args.output.with_suffix('.npy')))
-
+    
